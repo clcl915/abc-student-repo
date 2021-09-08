5bbcc00e13728a4dd736b98d99eaca99e5f1857e
clcl915 <cl4750@nyu.edu>
2021-09-08 22:09:19 +0800

    added cindy's room

---
+++ cindys-room/script.js
@@ -0,0 +1,5 @@
+let aboutMe = document.getElementById('aboutLink');
+let aboutMePopUp = document.getElementById('aboutPopUp');
+aboutMe.addEventListener("click", ()=>{
+  aboutMePopUp.innerHTML += "I'm a senior majoring in IMB! Currently in Shanghai";
+})
