11f3156d9aec71acf9379b5047fdd91ca760e67a
clcl915 <cl4750@nyu.edu>
2021-09-30 14:00:55 +0800

    changing project

--- projects/project-A/code/script.js
+++ projects/project-A/code/script.js
@@ -1,5 +1,5 @@
 let mobileMessage = document.getElementById('mobileMessage');
-
+var orientation = screen.orientation;
 
 function detectDevice(){
   console.log(!!navigator.maxTouchPoints)
@@ -19,35 +19,48 @@ function detectDevice(){
 
 detectDevice()
 
+var orientation = (screen.orientation || {}).type || screen.mozOrientation || screen.msOrientation;
+
+if (orientation === "landscape-primary") {
+  alert("That looks good.");
+} else if (orientation === "landscape-secondary") {
+  alert("Mmmh... the screen is upside down!");
+} else if (orientation === "portrait-secondary" || orientation === "portrait-primary") {
+  alert("Mmmh... you should rotate your device to landscape");
+} else if (orientation === undefined) {
+  alert("The orientation API isn't supported in this browser :(");
+}
+
+
 //has to connect with https
-if (window.DeviceOrientationEvent) {
-  window.alert("yes");
-  console.log("yes");
-  // window.addEventListener('deviceorientation', deviceOrientationHandler, false);
-  window.addEventListener("deviceorientation", function(e){
-    handler(e);
-}, true);
-  mobileMessage.innerHTML = "Supported!";
-  document.getElementsByClassName('square')[0].style.backgroundColor="green";
-}
-function handler(event) {
-  // event.preventDefault();
-  mobileMessage.style.color="green";
-  mobileMessage.innerHTML += "this works";
-  console.log("does this work");
-  var x = event.beta;
-  var y = event.gamma;
-  var width = window.outerWidth;
-  var rot = event.gamma / 360;
-  var left = ( width / 2 ) * rot;
-  console.log(left);
-  mobileMessage.style.left = left + 'px';
-  // $('.square').css({
-  //   'top':x,
-  //   'left': y
-  // });
-  mobileMessage.innerHTML += "do u work";
-}
+// if (window.DeviceOrientationEvent) {
+//   window.alert("yes");
+//   console.log("yes");
+//   // window.addEventListener('deviceorientation', deviceOrientationHandler, false);
+//   window.addEventListener("deviceorientation", function(e){
+//     handler(e);
+// }, true);
+//   mobileMessage.innerHTML = "Supported!";
+//   document.getElementsByClassName('square')[0].style.backgroundColor="green";
+// }
+// function handler(event) {
+//   // event.preventDefault();
+//   mobileMessage.style.color="green";
+//   mobileMessage.innerHTML += "this works";
+//   console.log("does this work");
+//   var x = event.beta;
+//   var y = event.gamma;
+//   var width = window.outerWidth;
+//   var rot = event.gamma / 360;
+//   var left = ( width / 2 ) * rot;
+//   console.log(left);
+//   mobileMessage.style.left = left + 'px';
+//   // $('.square').css({
+//   //   'top':x,
+//   //   'left': y
+//   // });
+//   mobileMessage.innerHTML += "do u work";
+// }
 // function deviceOrientation(event) {
 //   mobileMessage.innerHTML += "This is working!";
 //   alert(event.alpha + ' : ' + event.beta + ' : ' + event.gamma);
