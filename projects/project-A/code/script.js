87dac13de8bbd06522ad106e4498b793bbb9b22b
clcl915 <cl4750@nyu.edu>
2021-09-30 09:35:41 +0800

    test of orientation function

--- projects/project-A/code/script.js
+++ projects/project-A/code/script.js
@@ -27,28 +27,36 @@ if (window.DeviceOrientationEvent) {
   window.addEventListener("deviceorientation", orientation, true);
   mobileMessage.innerHTML = "Supported!";
 }
+function orientation(event) {
+  var x = event.beta;
+  var y = event.gamma;
 
-function deviceOrientation(event) {
-  mobileMessage.innerHTML += "This is working!";
-  alert(event.alpha + ' : ' + event.beta + ' : ' + event.gamma);
-  window.removeEventListener('deviceorientation', deviceOrientation);
-  mobileMessage.innerHTML += "This is still working!";
+  $('.mobileMessage').css({
+    'top':x,
+    'left': y
+  });
 }
-window.addEventListener('deviceorientation', deviceOrientation);
-
-if (window.DeviceOrientationEvent) {
-    window.addEventListener("deviceorientation", function(event) {
-        // alpha: rotation around z-axis
-        var rotateDegrees = event.alpha;
-        // gamma: left to right
-        var leftToRight = event.gamma;
-        // beta: front back motion
-        var frontToBack = event.beta;
-
-        handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
-    }, true);
-}
-
-var handleOrientationEvent = function(frontToBack, leftToRight, rotateDegrees) {
-    console.log("moving");
-};
+// function deviceOrientation(event) {
+//   mobileMessage.innerHTML += "This is working!";
+//   alert(event.alpha + ' : ' + event.beta + ' : ' + event.gamma);
+//   window.removeEventListener('deviceorientation', deviceOrientation);
+//   mobileMessage.innerHTML += "This is still working!";
+// }
+// window.addEventListener('deviceorientation', deviceOrientation);
+//
+// if (window.DeviceOrientationEvent) {
+//     window.addEventListener("deviceorientation", function(event) {
+//         // alpha: rotation around z-axis
+//         var rotateDegrees = event.alpha;
+//         // gamma: left to right
+//         var leftToRight = event.gamma;
+//         // beta: front back motion
+//         var frontToBack = event.beta;
+//
+//         handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
+//     }, true);
+// }
+//
+// var handleOrientationEvent = function(frontToBack, leftToRight, rotateDegrees) {
+//     console.log("moving");
+// };
