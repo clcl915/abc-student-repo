d1f5497788eb0f0e2b67c886ce7dcad429502220
clcl915 <cl4750@nyu.edu>
2021-10-28 19:22:43 +0800

    proj b - prodo home

--- projects/project-B/code/background.js
+++ projects/project-B/code/background.js
@@ -1,32 +1,196 @@
-console.log("i am background");
-let counter = 0;
+// what I need
+// - detect url change
+// - time spent on page -> to script.js
+// - detect idle, detect screen sleep, detect url change, tab change
+// - notification open popup html when complete
+// - change tiles to home
+// - storage add in progress of game
 
-chrome.storage.local.get(['counterInStorage'], function(result){
-  console.log("got this from storage", result);
-  console.log('value currently is '+ result.counterInStorage);
-  if (result.counterInStorage == "undefined"){
-    counter = 0;
-    chrome.storage.local.get(['counterInStorage'], function(result){
-      counter = result.counterInStorage;
-    });
-  }
-  counter = result.counterInStorage;
+console.log("i am background");
+chrome.extension.onConnect.addListener(function(port) {
+      console.log("Connected .....");
+      port.onMessage.addListener(function(msg) {
+           console.log("message recieved" + msg);
+           port.postMessage("Hi Popup.js");
+      });
+ })
+let localIntroStatus = false;
+let farmSuccess = false;let gardenSuccess = false;let buildSuccess = false;
+let taskfailed= false;
+chrome.storage.sync.set({finishedIntro: localIntroStatus,farmSuccess: farmSuccess,gardenSuccess: gardenSuccess,buildSuccess: buildSuccess},function(){
+  console.log("set localstorage");
 });
+// chrome.browserAction.onClicked.addListener(function (tab){
+//   console.log("browser icon clicked");
+// });
 
 function handleMessage(request, sender, sendResponse){
   console.log(request);
-  if (request.type=="increaseCounter"){
-    //increase local counter
-    counter++;
-    //increase counter in local storage
-    chrome.storage.local.set({counterInStorage: counter},function(){
-      console.log("Saved", counter, 'to local storage.');
+  if (request.type=="finishedIntro"){
+    //see if tabs changed
+    checkIntro();
+    localIntroStatus = true;
+    chrome.storage.sync.set({finishedIntro: localIntroStatus},function(){
+      console.log("finished introduction");
     });
   }
-  else if (request.type=="getCount"){
-    sendResponse(counter)
+  else if (request.type=="getStatus"){
+    var data = {};
+
+    //   console.log("key is" + key);
+    //   console.log(data);
+    //   let keystring = "'" + key +"'" ;
+    //   console.log("keystring is" + keystring);
+    //   let keyvalue= chrome.storage.local.get(function(result){console.log(result[key]);return result[key]});
+    //   console.log(keyvalue);
+    //   data[key] = keyvalue;
+    //   console.log("data is " + data[key]);
+    // });
+    // sendResponse({data: data});
+    // console.log(data);
+    // return true;
+    sendResponse(localIntroStatus);
+  }
+  else if (request.type=="choseFarm"){
+    //15mins
+    let taskfailed= false;
+    console.log("the user is farming");
+    chrome.alarms.create("1min", {
+      delayInMinutes: 1,
+      periodInMinutes: 1
+    });
+    // sendResponse(localIntroStatus);
+  }
+  else if (request.type=="choseGarden"){
+    //15mins
+    let taskfailed= false;
+    console.log("the user is gardening");
+    chrome.alarms.create("30min", {
+      delayInMinutes: 1,
+      periodInMinutes: 1
+    });
+    // sendResponse(localIntroStatus);
+  }
+  else if (request.type=="choseBuild"){
+    //15mins
+    let taskfailed= false;
+    console.log("the user is building");
+    chrome.alarms.create("1hr", {
+      delayInMinutes: 1,
+      periodInMinutes: 1
+    });
+    // sendResponse(localIntroStatus);
   }
   // console.log("counter in background", counter );
 }
+
+function checkIntro(){
+  chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    // alert('changed');
+    console.log("Updated tab (" + tabId + ")=>>> " + tab.url);
+  })
+}
+
+//detect if new tab was opened and send notification
+chrome.tabs.onCreated.addListener(function(tab) {
+   console.log("new tab created");
+   taskfailed = true;
+   chrome.notifications.create('', {
+     title: 'PRODO HOME',
+     message: 'Task failed! You opened a new tab :( \n Open your prodo home',
+     iconUrl: 'house.png',
+     type: 'basic'
+   });
+});
+
+chrome.idle.setDetectionInterval(300);
+chrome.idle.onStateChanged.addListener((newState)=>{
+  console.log("STATE CHANGED", newState);
+  let message = {type: newState}
+  chrome.runtime.sendMessage(message);
+  // chrome.browserAction.openPopup();
+  if (newState =="idle")
+  {
+    taskfailed = true;
+    chrome.notifications.create('', {
+      title: 'PRODO HOME',
+      message: 'Task failed! You opened a new tab :( \n Open your prodo home',
+      iconUrl: 'house.png',
+      type: 'basic'
+    });
+    console.log("sent");
+  }
+})
+chrome.alarms.onAlarm.addListener(function(alarm) {
+  if (alarm.name === "1min") {
+    if (taskfailed != true){
+      console.log("been a minute");
+      chrome.notifications.create('', {
+        title: 'PRODO HOME',
+        message: 'Farm Task success! \n Check on your prodo home!',
+        iconUrl: 'house.png',
+        type: 'basic'
+      });
+      farmSuccess = true;
+      chrome.storage.sync.set({farmSuccess: farmSuccess},function(){
+        console.log("finished 15mins farming");
+      });
+      chrome.extension.onConnect.addListener(function(port) {
+            console.log("Connected .....");
+            port.onMessage.addListener(function(msg) {
+                 console.log("message recieved" + msg);
+                 port.postMessage("farmsuccess");
+            });
+       })
+    }
+    chrome.alarms.clear("1min");
+  }
+  else if (alarm.name === "30min") {
+    if (taskfailed != true){
+      console.log("been 30 minutes");
+      chrome.notifications.create('', {
+        title: 'PRODO HOME',
+        message: 'Garden Task success! \n Check on your prodo home!',
+        iconUrl: 'house.png',
+        type: 'basic'
+      });
+      gardenSuccess = true;
+      chrome.storage.sync.set({gardenSuccess: gardenSuccess},function(){
+        console.log("finished 30mins gardening");
+      });
+      chrome.extension.onConnect.addListener(function(port) {
+            console.log("Connected .....");
+            port.onMessage.addListener(function(msg) {
+                 console.log("message recieved" + msg);
+                 port.postMessage("gardensuccess");
+            });
+       });
+     }
+    chrome.alarms.clear("30min");
+  }
+  else if (alarm.name === "1hr") {
+    if (taskfailed != true){
+      console.log("been 1 hr");
+      chrome.notifications.create('', {
+        title: 'PRODO HOME',
+        message: 'Build Task success! \n Check on your prodo home!',
+        iconUrl: 'house.png',
+        type: 'basic'
+      });
+      gardenSuccess = true;
+      chrome.storage.sync.set({buildSuccess: buildSuccess},function(){
+        console.log("finished 1 hr build");
+      });
+      chrome.extension.onConnect.addListener(function(port) {
+            console.log("Connected .....");
+            port.onMessage.addListener(function(msg) {
+                 console.log("message recieved" + msg);
+                 port.postMessage("buildsuccess");
+            });
+       });
+      }
+    chrome.alarms.clear("1hr");
+  }
+});
 
 chrome.runtime.onMessage.addListener(handleMessage);
--- projects/project-B/code/popup/script.js
+++ projects/project-B/code/popup/script.js
@@ -1,18 +1,156 @@
-let button = document.querySelector('#button');
-let count = document.querySelector('#count');
-let counter=0;
+let pigoverlay = document.getElementsByClassName('pigoverlay')[0];
+let house = document.getElementsByClassName('house')[0];
+var port = chrome.extension.connect({
+        name: "Sample Communication"
+   });
+   port.postMessage("Hi BackGround");
+   port.onMessage.addListener(function(msg) {
+     if (msg == 'farmsuccess'){
+       console.log("farm success");
+       pigoverlay.classList.remove("hidden");
+     }
+     else if (msg == 'gardensuccess'){
+       console.log("garden success");
+       block1.style.backgroundPosition = "right";
+       block1.style.backgroundImage = "url('https://64.media.tumblr.com/5c36943adcece7769082d82863cb0e4e/9e54f617e5091267-a5/s500x750/ebc0ffc172c9ed79204b64f95b5814b34f5330a0.png')";
+     }
+     else if (msg == 'buildsuccess'){
+       console.log("build success");
+       house.classList.remove("hidden");
+     }
+        console.log("message recieved" + msg);
+ });
+let block1 = document.getElementsByClassName('1')[0];
+let ontop = document.getElementsByClassName('ontop')[0];
+let idle = document.getElementsByClassName('idle')[0];
+let popupMessage = document.getElementById('popupMessage');
+let next = document.getElementsByClassName('next')[0];
+let confirmGame = document.getElementsByClassName('confirmGame')[0];
+let yes = document.getElementsByClassName('yes')[0];
+let no = document.getElementsByClassName('no')[0];
+let messageCommands = document.getElementsByClassName('messageCommands')[0];
+let selectTasks = document.getElementsByClassName('selectTasks')[0];
+let charMessages = ["This is a space for you to build.", "We are your little helpers ready to help you.", "Your little helpers will do the work for you in the background.", "Your job is to just be PRODUCTIVE!", "All you have to do is decide to stay on one page, press on our chrome extension icon, and work!", "But, you can't change tabs or be idle for too long.","If you stop working, we stop working.","Choose a time frame for your work and we will get to work as well!","Are you ready?"];
+let clicks = 0;
+let runCharacterAnimation, showMessage;
+let farm = document.getElementById('farm');
+let garden = document.getElementById('garden');
+let build = document.getElementById('build');
 
-let message={type:"getCount"};
-
+let message={type:"getStatus", keys: ["finishedIntro", "farmSuccess","gardenSuccess","buildSuccess"]};
+chrome.runtime.onMessage.addListener(function(message, messageSender, sendResponse) {
+  console.log(message);
+  console.log("hi");
+    if (message == 'farmsuccess'){
+      console.log("farm success");
+      block1.style.backgroundImage = "url('https://64.media.tumblr.com/5c36943adcece7769082d82863cb0e4e/9e54f617e5091267-a5/s500x750/ebc0ffc172c9ed79204b64f95b5814b34f5330a0.png')";
+    }
+});
 chrome.runtime.sendMessage(message,(response)=>{
+  console.log("background script");
   console.log("background script sent me this",response);
-  counter=response;
-  count.innerHTML = counter;
+  if (response == false){
+    introduction();
+  }
+  else{
+    selectTask();
+  }
 });
 
-button.addEventListener("click", ()=>{
-  counter++;
-  let message={type:"increaseCounter"};
-  chrome.runtime.sendMessage(message);
-  count.innerHTML=counter;
-});
+// INTRODUCTION -> Show Intro Message
+function introduction(){
+  runCharacterAnimation = setInterval(()=>{
+    console.log("walking");
+    idle.className = 'characterWalking';
+  },2500);
+
+  showMessage = setInterval(()=>{
+    clearInterval(runCharacterAnimation);
+    popupMessage.classList.remove("hidden");
+    next.classList.remove("hidden");
+  },5500);
+
+  next.addEventListener("click",()=>{
+    clearInterval(showMessage);
+    console.log("clicks is " + clicks);
+    console.log("message is " + charMessages.length);
+    if (clicks != (charMessages.length-1))
+      popupMessage.innerHTML = charMessages[clicks];
+    else{
+      popupMessage.innerHTML = charMessages[clicks];
+      confirmGame.classList.remove("hidden");
+      yes.classList.remove("hidden");
+      no.classList.remove("hidden");
+      next.classList.add("hidden");
+    }
+    clicks ++;
+  });
+
+  yes.addEventListener("click",()=>{
+    popupMessage.innerHTML = "Ok! Now, go to a site you want to be productive at. Click my icon again when you're done.";
+    confirmGame.classList.add("hidden");
+    yes.classList.add("hidden");
+    no.classList.add("hidden");
+    let message = {type: "finishedIntro"}
+    chrome.runtime.sendMessage(message);
+  });
+
+  no.addEventListener("click",()=>{
+    confirmGame.classList.add("hidden");
+    popupMessage.innerHTML = ".... <br> Ok. Bye.";
+    setInterval(()=>{window.close();},1500);
+  });
+}
+
+function selectTask(){
+  idle.className = 'characterWalking';
+  showMessage = setInterval(()=>{
+    popupMessage.classList.remove("hidden");
+    popupMessage.innerHTML = "Ready to get to work on this site?";
+    confirmGame.classList.remove("hidden");
+    yes.classList.remove("hidden");
+    no.classList.remove("hidden");
+  },2500);
+
+  yes.addEventListener("click",()=>{
+    clearInterval(showMessage);
+    popupMessage.innerHTML = "Ok! Select your task";
+    popupMessage.style.top = 0;
+    selectTasks.classList.remove("hidden");
+    confirmGame.classList.add("hidden");
+    yes.classList.add("hidden");
+    no.classList.add("hidden");
+  });
+  no.addEventListener("click",()=>{
+    popupMessage.innerHTML = "Ok. Come back when you're ready!";
+    setInterval(()=>{window.close();},1500);
+  });
+  // select tasks 15m/30m/1hr
+  farm.addEventListener("click",()=>{
+    console.log("farm selected");
+    let message = {type: "choseFarm"}
+    chrome.runtime.sendMessage(message);
+    selectTasks.classList.add("hidden");
+    popupMessage.innerHTML = "Ok! Let's get to work! See you in 15 mins";
+    popupMessage.style.top = "25%";
+    setInterval(()=>{window.close();},2500);
+  });
+  garden.addEventListener("click",()=>{
+    console.log("garden selected");
+    let message = {type: "choseGarden"}
+    chrome.runtime.sendMessage(message);
+    selectTasks.classList.add("hidden");
+    popupMessage.innerHTML = "Ok! Let's get to work! See you in 30 mins";
+    popupMessage.style.top = "25%";
+    setInterval(()=>{window.close();},2500);
+  });
+  build.addEventListener("click",()=>{
+    console.log("build selected");
+    let message = {type: "choseBuild"}
+    chrome.runtime.sendMessage(message);
+    selectTasks.classList.add("hidden");
+    popupMessage.innerHTML = "Ok! Let's get to work! See you in 1 hour";
+    popupMessage.style.top = "25%";
+    setInterval(()=>{window.close();},2500);
+  });
+}
